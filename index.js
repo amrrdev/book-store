5809273d731812c05e7ed80e70161898c5bdf200
Amr Mubarak <amrrdev@gmail.com>
2025-07-18 22:17:32 +0300

    feat: add dotenv for environment variable management and connect to MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
 
 const app = express();
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(() => console.log(`MongoDB connection error: ${err}`));
 
 app.listen(8000, () => {
   console.log("Server is running on http://localhost:8000");
